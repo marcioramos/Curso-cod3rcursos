78746d4e9724ec79e1a1e5de3a5b8ee249799381
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-07-19 17:49:02 -0300

    Aula 37

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'restaurants', component: RestaurantsComponent},
   { path: 'about', component: AboutComponent}
 ];
