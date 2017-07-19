20425a1b1023158ffc3da39104ef71c2abeaccb1
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-07-19 20:06:11 -0300

    aula 48

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,20 @@ import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurants', component: RestaurantsComponent },
-  { path: 'restaurants/:id', component: RestaurantDetailComponent },
+  {
+    path: 'restaurants/:id',
+    component: RestaurantDetailComponent,
+    children: [
+      { path: '', redirectTo: 'menu', pathMatch: 'full' },
+      { path: 'menu', component: MenuComponent },
+      { path: 'reviews', component: ReviewsComponent }
+    ]
+  },
   { path: 'about', component: AboutComponent }
 ];
