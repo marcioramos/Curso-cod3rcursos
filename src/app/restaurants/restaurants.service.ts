b9edfc71545b1eb812cd360ce827b3189f06a987
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-07-24 22:37:03 -0300

    Aula 106

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -15,8 +15,8 @@ export class RestaurantsService {
 
   constructor(private http: Http) { }
 
-  restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${url_api}/restaurants`)
+  restaurants(search?: string): Observable<Restaurant[]> {
+    return this.http.get(`${url_api}/restaurants`, { params: { q: search } })
     .map(response => response.json())
     .catch(ErrorHandler.handleError);
   }
